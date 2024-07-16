83a14f3e3586cb347ba1d8d3554840d8f18f4a95
huzaifa1214 <huzaifaimran1998@gmail.com>
2024-07-16 21:50:52 +0400

    frontend: UI improved

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -35,11 +35,15 @@ const Landing = () => {
       <div>
         <video autoPlay ref={videoRef}></video>
         <input
+          className="h-10 rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 w-20%"
           type="text"
+          placeholder="Enter your name"
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
-        <button onClick={() => setJoined(true)}>Join</button>
+        <button className="pl-10" onClick={() => setJoined(true)}>
+          Join
+        </button>
       </div>
     );
   }
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Add this line to include your React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
